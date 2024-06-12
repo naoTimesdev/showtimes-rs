3eda3ffebfb85095a9234c548ca90c124b10c431
noaione <noaione0809@gmail.com>
2024-06-12 13:09:53 +0700

    feat(fs): implement S3 target

--- showtimes_fs/src/lib.rs
+++ showtimes_fs/src/lib.rs
@@ -1,14 +1,100 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use tokio::io::{AsyncRead, AsyncWrite};
+
+pub mod s3;
+
+/// The list of "pool" type of the filesystems.
+///
+/// Currently only `LocalFs` and `S3Fs` are supported.
+pub enum FsPool {
+    LocalFs,
+    S3Fs(crate::s3::S3Fs),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// The kind of the file system object.
+#[derive(Default, Debug, Clone, tosho_macros::EnumName)]
+pub enum FsFileKind {
+    #[default]
+    Images,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+/// The file object in the filesystem.
+#[derive(Debug, Clone)]
+pub struct FsFileObject {
+    /// The filename of the file.
+    pub filename: String,
+    /// The content type of the file.
+    pub content_type: String,
+    /// The size of the file.
+    pub size: i64,
+    /// The last modified time of the file.
+    pub last_modified: Option<chrono::DateTime<chrono::Utc>>,
+}
+
+/// Base trait for the filesystem implementations.
+///
+/// Implement some of the basic operations needed for it to work with Showtimes.
+pub trait FsImpl {
+    /// Initialize the filesystem.
+    ///
+    /// This can be used to test if the filesystem is working correctly.
+    fn init(&self) -> impl std::future::Future<Output = anyhow::Result<()>> + Send;
+    /// Stat or get a file information in the filesystem.
+    fn file_stat(
+        &self,
+        base_key: &str,
+        filename: &str,
+        parent_id: Option<&str>,
+        kind: Option<FsFileKind>,
+    ) -> impl std::future::Future<Output = anyhow::Result<FsFileObject>> + Send;
+    /// Check if a file exists in the filesystem.
+    fn file_exists(
+        &self,
+        base_key: &str,
+        filename: &str,
+        parent_id: Option<&str>,
+        kind: Option<FsFileKind>,
+    ) -> impl std::future::Future<Output = anyhow::Result<bool>> + Send;
+    /// Upload a file to the filesystem.
+    fn file_stream_upload<R: AsyncRead + Unpin + Send>(
+        &self,
+        base_key: &str,
+        filename: &str,
+        stream: &mut R,
+        parent_id: Option<&str>,
+        kind: Option<FsFileKind>,
+    ) -> impl std::future::Future<Output = anyhow::Result<FsFileObject>> + Send;
+    /// Download a file from the filesystem.
+    fn file_stream_download<W: AsyncWrite + Unpin + Send>(
+        &self,
+        base_key: &str,
+        filename: &str,
+        writer: &mut W,
+        parent_id: Option<&str>,
+        kind: Option<FsFileKind>,
+    ) -> impl std::future::Future<Output = anyhow::Result<()>> + Send;
+    /// Delete a file from the filesystem.
+    fn file_delete(
+        &self,
+        base_key: &str,
+        filename: &str,
+        parent_id: Option<&str>,
+        kind: Option<FsFileKind>,
+    ) -> impl std::future::Future<Output = anyhow::Result<()>> + Send;
+}
+
+/// Make a file path from the base key, filename, parent id, and kind.
+pub(crate) fn make_file_path(
+    base_key: &str,
+    filename: &str,
+    parent_id: Option<&str>,
+    kind: Option<FsFileKind>,
+) -> String {
+    let kind = kind.unwrap_or_default().to_name().to_ascii_lowercase();
+
+    let mut path = format!("{}/{}/", kind, base_key);
+    if let Some(parent_id) = parent_id {
+        path.push_str(&format!("{}/", parent_id.replace('-', "")))
     }
+    path.push_str(filename);
+    path
 }
---
+++ showtimes_fs/src/s3.rs
@@ -0,0 +1,128 @@
+use s3::{creds::Credentials, Bucket};
+use tokio::io::{AsyncRead, AsyncWrite};
+
+use crate::{make_file_path, FsFileKind, FsFileObject, FsImpl};
+
+#[derive(Debug, Clone)]
+pub struct S3Fs {
+    bucket: Bucket,
+}
+
+impl S3Fs {
+    /// Create a new instance of [`S3Fs`] filesystem implementation.
+    ///
+    /// # Parameters
+    /// * `bucket`: The name of the bucket.
+    /// * `access_key`: The access key for the bucket.
+    /// * `secret_key`: The secret key for the bucket.
+    /// * `region`: The region of the bucket.
+    pub fn new(bucket: &str, access_key: &str, secret_key: &str, region: ::s3::Region) -> Self {
+        let credentials =
+            Credentials::new(Some(access_key), Some(secret_key), None, None, None).unwrap();
+        let bucket = Bucket::new(bucket, region.clone(), credentials).unwrap();
+
+        Self { bucket }
+    }
+}
+
+impl FsImpl for S3Fs {
+    async fn init(&self) -> anyhow::Result<()> {
+        // Test if the bucket exists
+        self.bucket
+            .list("/".to_string(), Some("/".to_string()))
+            .await?;
+
+        Ok(())
+    }
+
+    async fn file_stat(
+        &self,
+        base_key: &str,
+        filename: &str,
+        parent_id: Option<&str>,
+        kind: Option<FsFileKind>,
+    ) -> anyhow::Result<FsFileObject> {
+        let key = make_file_path(base_key, filename, parent_id, kind);
+        let (object, _) = self.bucket.head_object(&key).await?;
+
+        let content_type = match object.content_type {
+            Some(content_type) => content_type,
+            None => {
+                let guessed = mime_guess::from_path(filename);
+                guessed.first_or_octet_stream().to_string()
+            }
+        };
+
+        let last_modified = match object.last_modified {
+            Some(last_modified) => {
+                // last_modified is HTTP Last-Modified header
+                match chrono::DateTime::parse_from_rfc2822(&last_modified) {
+                    Ok(dt) => Some(dt.with_timezone(&chrono::Utc)),
+                    Err(_) => None,
+                }
+            }
+            None => None,
+        };
+
+        Ok(FsFileObject {
+            filename: key,
+            content_type,
+            size: object.content_length.unwrap_or(-1),
+            last_modified,
+        })
+    }
+
+    async fn file_exists(
+        &self,
+        base_key: &str,
+        filename: &str,
+        parent_id: Option<&str>,
+        kind: Option<FsFileKind>,
+    ) -> anyhow::Result<bool> {
+        let key = make_file_path(base_key, filename, parent_id, kind);
+        let (object, _) = self.bucket.head_object(&key).await?;
+
+        Ok(object.content_length.unwrap_or(-1) > 0)
+    }
+
+    async fn file_stream_upload<R: AsyncRead + Unpin + Send>(
+        &self,
+        base_key: &str,
+        filename: &str,
+        stream: &mut R,
+        parent_id: Option<&str>,
+        kind: Option<FsFileKind>,
+    ) -> anyhow::Result<FsFileObject> {
+        let key = make_file_path(base_key, filename, parent_id, kind.clone());
+        self.bucket.put_object_stream(stream, &key).await?;
+
+        self.file_stat(base_key, filename, parent_id, kind).await
+    }
+
+    async fn file_stream_download<W: AsyncWrite + Unpin + Send>(
+        &self,
+        base_key: &str,
+        filename: &str,
+        writer: &mut W,
+        parent_id: Option<&str>,
+        kind: Option<FsFileKind>,
+    ) -> anyhow::Result<()> {
+        let key = make_file_path(base_key, filename, parent_id, kind);
+        self.bucket.get_object_to_writer(&key, writer).await?;
+
+        Ok(())
+    }
+
+    async fn file_delete(
+        &self,
+        base_key: &str,
+        filename: &str,
+        parent_id: Option<&str>,
+        kind: Option<FsFileKind>,
+    ) -> anyhow::Result<()> {
+        let key = make_file_path(base_key, filename, parent_id, kind);
+        self.bucket.delete_object(&key).await?;
+
+        Ok(())
+    }
+}
