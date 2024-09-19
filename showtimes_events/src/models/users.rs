551a621da2830ec8bad033728709ea7ba8926732
noaione <noaione0809@gmail.com>
2024-09-19 12:44:39 +0700

    refactor(events): use the new event model

--- showtimes_events/src/models/users.rs
+++ showtimes_events/src/models/users.rs
@@ -1,9 +1,11 @@
 use serde::{Deserialize, Serialize};
+use showtimes_derive::EventModel;
 
 /// A user created event
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, EventModel)]
 pub struct UserCreatedEvent {
     #[serde(with = "showtimes_shared::ulid_serializer")]
+    #[event_copy]
     id: showtimes_shared::ulid::Ulid,
     username: String,
 }
@@ -26,22 +28,18 @@ impl From<&showtimes_db::m::User> for UserCreatedEvent {
     }
 }
 
-impl UserCreatedEvent {
-    pub fn id(&self) -> showtimes_shared::ulid::Ulid {
-        self.id
-    }
-}
-
 /// A user updated data event
 ///
 /// Used in conjuction with the [`UserUpdatedEvent`]
-#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+#[derive(Debug, Clone, Serialize, Deserialize, Default, EventModel)]
 pub struct UserUpdatedDataEvent {
     #[serde(skip_serializing_if = "Option::is_none")]
     name: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[event_copy]
     api_key: Option<showtimes_shared::APIKey>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[event_copy]
     kind: Option<showtimes_db::m::UserKind>,
     #[serde(skip_serializing_if = "Option::is_none")]
     avatar: Option<showtimes_db::m::ImageMetadata>,
@@ -49,52 +47,11 @@ pub struct UserUpdatedDataEvent {
     discord_meta: Option<showtimes_db::m::DiscordUser>,
 }
 
-impl UserUpdatedDataEvent {
-    pub fn name(&self) -> Option<&str> {
-        self.name.as_deref()
-    }
-
-    pub fn api_key(&self) -> Option<&showtimes_shared::APIKey> {
-        self.api_key.as_ref()
-    }
-
-    pub fn kind(&self) -> Option<showtimes_db::m::UserKind> {
-        self.kind
-    }
-
-    pub fn avatar(&self) -> Option<&showtimes_db::m::ImageMetadata> {
-        self.avatar.as_ref()
-    }
-
-    pub fn discord_meta(&self) -> Option<&showtimes_db::m::DiscordUser> {
-        self.discord_meta.as_ref()
-    }
-
-    pub fn set_name(&mut self, name: impl Into<String>) {
-        self.name = Some(name.into());
-    }
-
-    pub fn set_api_key(&mut self, api_key: showtimes_shared::APIKey) {
-        self.api_key = Some(api_key);
-    }
-
-    pub fn set_kind(&mut self, kind: showtimes_db::m::UserKind) {
-        self.kind = Some(kind);
-    }
-
-    pub fn set_avatar(&mut self, avatar: &showtimes_db::m::ImageMetadata) {
-        self.avatar = Some(avatar.clone());
-    }
-
-    pub fn set_discord_meta(&mut self, discord_meta: &showtimes_db::m::DiscordUser) {
-        self.discord_meta = Some(discord_meta.clone());
-    }
-}
-
 /// A user updated event
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, EventModel)]
 pub struct UserUpdatedEvent {
     #[serde(with = "showtimes_shared::ulid_serializer")]
+    #[event_copy]
     id: showtimes_shared::ulid::Ulid,
     before: UserUpdatedDataEvent,
     after: UserUpdatedDataEvent,
@@ -108,16 +65,30 @@ impl UserUpdatedEvent {
     ) -> Self {
         Self { id, before, after }
     }
+}
 
-    pub fn id(&self) -> showtimes_shared::ulid::Ulid {
-        self.id
+/// A user deleted event
+#[derive(Debug, Clone, Serialize, Deserialize, EventModel)]
+pub struct UserDeletedEvent {
+    #[serde(with = "showtimes_shared::ulid_serializer")]
+    #[event_copy]
+    id: showtimes_shared::ulid::Ulid,
+}
+
+impl UserDeletedEvent {
+    pub fn new(id: showtimes_shared::ulid::Ulid) -> Self {
+        Self { id }
     }
+}
 
-    pub fn before(&self) -> &UserUpdatedDataEvent {
-        &self.before
+impl From<showtimes_db::m::User> for UserDeletedEvent {
+    fn from(user: showtimes_db::m::User) -> Self {
+        Self { id: user.id }
     }
+}
 
-    pub fn after(&self) -> &UserUpdatedDataEvent {
-        &self.after
+impl From<&showtimes_db::m::User> for UserDeletedEvent {
+    fn from(user: &showtimes_db::m::User) -> Self {
+        Self { id: user.id }
     }
 }
