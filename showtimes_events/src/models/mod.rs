eae4c2463e40583c2b228dcd62f5cd5032ab98ec
noaione <noaione0809@gmail.com>
2024-09-19 12:44:59 +0700

    feat(events): add servers update event

--- showtimes_events/src/models/mod.rs
+++ showtimes_events/src/models/mod.rs
@@ -1,5 +1,7 @@
 pub mod common;
+pub mod servers;
 pub mod users;
 
 pub use common::*;
+pub use servers::*;
 pub use users::*;
---
+++ showtimes_events/src/models/servers.rs
@@ -0,0 +1,90 @@
+use serde::{Deserialize, Serialize};
+use showtimes_derive::EventModel;
+
+/// A server created event
+#[derive(Debug, Clone, Serialize, Deserialize, EventModel)]
+pub struct ServerCreatedEvent {
+    #[serde(with = "showtimes_shared::ulid_serializer")]
+    #[event_copy]
+    id: showtimes_shared::ulid::Ulid,
+    name: String,
+}
+
+impl From<showtimes_db::m::Server> for ServerCreatedEvent {
+    fn from(value: showtimes_db::m::Server) -> Self {
+        Self {
+            id: value.id,
+            name: value.name,
+        }
+    }
+}
+
+impl From<&showtimes_db::m::Server> for ServerCreatedEvent {
+    fn from(value: &showtimes_db::m::Server) -> Self {
+        Self {
+            id: value.id,
+            name: value.name.clone(),
+        }
+    }
+}
+
+/// A server updated data event
+///
+/// Used in conjuction with the [`ServerUpdatedEvent`]
+#[derive(Debug, Clone, Serialize, Deserialize, Default, EventModel)]
+pub struct ServerUpdatedDataEvent {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    integrations: Option<Vec<showtimes_db::m::IntegrationId>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    owners: Option<Vec<showtimes_db::m::ServerUser>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    avatar: Option<showtimes_db::m::ImageMetadata>,
+}
+
+/// A server updated event
+#[derive(Debug, Clone, Serialize, Deserialize, EventModel)]
+pub struct ServerUpdatedEvent {
+    #[serde(with = "showtimes_shared::ulid_serializer")]
+    #[event_copy]
+    id: showtimes_shared::ulid::Ulid,
+    before: ServerUpdatedDataEvent,
+    after: ServerUpdatedDataEvent,
+}
+
+impl ServerUpdatedEvent {
+    pub fn new(
+        id: showtimes_shared::ulid::Ulid,
+        before: ServerUpdatedDataEvent,
+        after: ServerUpdatedDataEvent,
+    ) -> Self {
+        Self { id, before, after }
+    }
+}
+
+/// A server deleted event
+#[derive(Debug, Clone, Serialize, Deserialize, EventModel)]
+pub struct ServerDeletedEvent {
+    #[serde(with = "showtimes_shared::ulid_serializer")]
+    #[event_copy]
+    id: showtimes_shared::ulid::Ulid,
+}
+
+impl ServerDeletedEvent {
+    pub fn new(id: showtimes_shared::ulid::Ulid) -> Self {
+        Self { id }
+    }
+}
+
+impl From<showtimes_db::m::Server> for ServerDeletedEvent {
+    fn from(value: showtimes_db::m::Server) -> Self {
+        Self { id: value.id }
+    }
+}
+
+impl From<&showtimes_db::m::Server> for ServerDeletedEvent {
+    fn from(value: &showtimes_db::m::Server) -> Self {
+        Self { id: value.id }
+    }
+}
